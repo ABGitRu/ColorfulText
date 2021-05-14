5e118ef9de8cc8642b5131b211dd66253f3d6b29
Alexey Barinov <begoodbebad2012@gmail.com>
2021-05-14 19:51:31 +0300

    init

--- ColorfulText/ViewController.swift
+++ ColorfulText/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        timer()
     }
-
-
+    
+    private func timer() {
+        Timer.scheduledTimer(
+            timeInterval: 0.2,
+            target: self,
+            selector: #selector(changeColor),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc private func changeColor() {
+        helloWorldLabel.textColor = .random()
+    }
+    
+    
 }
-
+// MARK: - CGFloat & UIColor Extenstions
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+            red:   .random(),
+            green: .random(),
+            blue:  .random(),
+            alpha: 1.0
+        )
+    }
+}
